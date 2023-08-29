5fa0be13505f1dfa839102bfe2c7cad9065ef881
Siwat Pruksapanya <siwat.pru@gmail.com>
2023-08-29 18:21:39 +0700

    Change mapping for closing buffer to zx

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -92,7 +92,7 @@ M.tabufline = {
     },
 
     -- close buffer + hide terminal buffer
-    ["<leader>x"] = {
+    ["zx"] = {
       function()
         require("nvchad.tabufline").close_buffer()
       end,
