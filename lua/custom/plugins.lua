63e307b85fd2af7ff32fa5b78018eb78ca07077a
Siwat Pruksapanya <siwat.pru@gmail.com>
2023-08-29 18:22:03 +0700

    Add git-worktree.nvim plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -93,6 +93,30 @@ local plugins = {
       vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
       vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
     end,
+  },
+  {
+    "siwatpru/git-worktree.nvim",
+    lazy = false,
+    branch="catch-and-handle-telescope-related-error",
+    config = function()
+      require("telescope").load_extension("git_worktree")
+
+      require("git-worktree").setup({
+        change_directory_command = "cd",
+          update_on_change = true,
+          update_on_change_command = "e .",
+          clearjumps_on_change = true,
+          autopush = false
+      })
+
+      -- :lua 
+      vim.keymap.set("n", "<leader>gw", function()
+        require('telescope').extensions.git_worktree.git_worktrees()
+      end)
+      vim.keymap.set("n", "<leader>ga", function()
+        require('telescope').extensions.git_worktree.create_git_worktree()
+      end)
+    end,
   }
   -- To make a plugin not be loaded
   -- {
